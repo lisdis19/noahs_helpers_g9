a7c472e550cdc83e81e539ebd9c83b647d3d45ad
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2025-11-02 19:00:45 -0500

    setup initial UI with landscape

--- core/engine.py
+++ core/engine.py
@@ -39,110 +39,114 @@ class Engine:
 
         return in_sight
 
+
+    def run_turn(self, time_elapsed: int) -> None:
+        is_raining = time_elapsed >= self.time - c.START_RAIN
+        ark_view = self.ark.get_view()
+
+        print(f"time_elapsed: {time_elapsed}, is_raining: {is_raining}")
+
+        # 1. show helpers their new surroundings:
+        # a their position
+        # b animals and helpers within 5km sight
+        # c turn number
+        # d whether it is raining
+        # e their flock
+        # f if they're in the Ark cell, the current view of the Ark BEFORE unloading any helpers' flock currently in the cell into it.
+
+        # 2. get helpers' one byte message:
+
+        sights = self._get_sights()
+        messages_to: dict[Player, list[Message]] = {
+            helper: [] for helper in self.helpers
+        }
+
+        for helper in self.helpers:
+            sight = Sight(helper.position, self.grid)
+
+            helper_ark_view = None
+            if helper.is_in_ark():
+                helper_ark_view = ark_view
+
+            snapshot = HelperSurroundingsSnapshot(
+                time_elapsed, is_raining, helper.position, sight, helper_ark_view
+            )
+            one_byte_message = helper.check_surroundings(snapshot)
+            if not (0 <= one_byte_message < c.ONE_BYTE):
+                raise Exception(
+                    f"helper {helper.id} gave incorrect message: {one_byte_message}"
+                )
+
+            # broadcast message to all neighbors
+            for neighbor in sights[helper]:
+                msg = Message(helper.get_view(), one_byte_message)
+                messages_to[neighbor].append(msg)
+
+        # 3. broadcast helpers' one byte message to all other helpers in their sight.
+
+        # 4. Let helpers take action on their surroundings:
+        # a obtain and/or release animals in their sight
+        # b move in any direction
+
+        for helper in self.helpers:
+            action = helper.get_action(messages_to[helper])
+
+            match action:
+                case Release(animal=a):
+                    print(f"{helper.id}: relasing {a}")
+                    helper_x, helper_y = tuple(map(int, helper.position))
+                    cell = self.grid[helper_y][helper_x]
+
+                    if a not in helper.flock:
+                        raise Exception(
+                            f"animal {a} not in helper {helper.id}'s flock"
+                        )
+
+                    helper.flock.remove(a)
+                    self.free_animals[a] = cell
+
+                case Obtain(animal=a):
+                    print(f"{helper.id}: obtaining {a}")
+
+                    helper_x, helper_y = tuple(map(int, helper.position))
+                    helper_cell = self.grid[helper_y][helper_x]
+
+                    if len(helper.flock) >= c.MAX_FLOCK_SIZE:
+                        raise Exception(
+                            f"helper {helper.id} tried to obtain animal with full flock"
+                        )
+
+                    if a not in helper_cell.animals:
+                        raise Exception(
+                            f"animal {a} not in helper {helper.id}'s cell {(helper_x, helper_y)}"
+                        )
+
+                    helper_cell.animals.remove(a)
+                    del self.free_animals[a]
+                    helper.flock.add(a)
+
+                case Move(x=x, y=y):
+                    print(f"{helper.id}: moving to {(x, y)}")
+
+                    if not helper.can_move_to(x, y):
+                        raise Exception(
+                            f"player cannot move from {helper.position} to {(x, y)}"
+                        )
+
+                    helper.position = (x, y)
+
+        # 5. let free animals move with `ANIMAL_MOVE_PROBABILITY` probability
+        for animal, cell in self.free_animals.items():
+            if random() < c.ANIMAL_MOVE_PROBABILITY:
+                neighbor = choice(cell.get_emptiest_neighbors())
+
+                print(f"{animal} moved: {cell.x, cell.y} -> {neighbor.x, neighbor.y}")
+
+                cell.animals.remove(animal)
+                neighbor.animals.add(animal)
+                # this is ok as we're not changing the keys
+                self.free_animals[animal] = neighbor
+
     def run_simulation(self) -> None:
         for time_elapsed in range(self.time):
-            is_raining = time_elapsed >= self.time - c.START_RAIN
-            ark_view = self.ark.get_view()
-
-            print(f"time_elapsed: {time_elapsed}, is_raining: {is_raining}")
-
-            # 1. show helpers their new surroundings:
-            # a their position
-            # b animals and helpers within 5km sight
-            # c turn number
-            # d whether it is raining
-            # e their flock
-            # f if they're in the Ark cell, the current view of the Ark BEFORE unloading any helpers' flock currently in the cell into it.
-
-            # 2. get helpers' one byte message:
-
-            sights = self._get_sights()
-            messages_to: dict[Player, list[Message]] = {
-                helper: [] for helper in self.helpers
-            }
-
-            for helper in self.helpers:
-                sight = Sight(helper.position, self.grid)
-
-                helper_ark_view = None
-                if helper.is_in_ark():
-                    helper_ark_view = ark_view
-
-                snapshot = HelperSurroundingsSnapshot(
-                    time_elapsed, is_raining, helper.position, sight, helper_ark_view
-                )
-                one_byte_message = helper.check_surroundings(snapshot)
-                if not (0 <= one_byte_message < c.ONE_BYTE):
-                    raise Exception(
-                        f"helper {helper.id} gave incorrect message: {one_byte_message}"
-                    )
-
-                # broadcast message to all neighbors
-                for neighbor in sights[helper]:
-                    msg = Message(helper.get_view(), one_byte_message)
-                    messages_to[neighbor].append(msg)
-
-            # 3. broadcast helpers' one byte message to all other helpers in their sight.
-
-            # 4. Let helpers take action on their surroundings:
-            # a obtain and/or release animals in their sight
-            # b move in any direction
-
-            for helper in self.helpers:
-                action = helper.get_action(messages_to[helper])
-
-                match action:
-                    case Release(animal=a):
-                        print(f"{helper.id}: relasing {a}")
-                        helper_x, helper_y = tuple(map(int, helper.position))
-                        cell = self.grid[helper_y][helper_x]
-
-                        if a not in helper.flock:
-                            raise Exception(
-                                f"animal {a} not in helper {helper.id}'s flock"
-                            )
-
-                        helper.flock.remove(a)
-                        self.free_animals[a] = cell
-
-                    case Obtain(animal=a):
-                        print(f"{helper.id}: obtaining {a}")
-
-                        helper_x, helper_y = tuple(map(int, helper.position))
-                        helper_cell = self.grid[helper_y][helper_x]
-
-                        if len(helper.flock) >= c.MAX_FLOCK_SIZE:
-                            raise Exception(
-                                f"helper {helper.id} tried to obtain animal with full flock"
-                            )
-
-                        if a not in helper_cell.animals:
-                            raise Exception(
-                                f"animal {a} not in helper {helper.id}'s cell {(helper_x, helper_y)}"
-                            )
-
-                        helper_cell.animals.remove(a)
-                        del self.free_animals[a]
-                        helper.flock.add(a)
-
-                    case Move(x=x, y=y):
-                        print(f"{helper.id}: moving to {(x, y)}")
-
-                        if not helper.can_move_to(x, y):
-                            raise Exception(
-                                f"player cannot move from {helper.position} to {(x, y)}"
-                            )
-
-                        helper.position = (x, y)
-
-            # 5. let free animals move with 0.5 probability
-            for animal, cell in self.free_animals.items():
-                if random() < c.ANIMAL_MOVE_PROBABILITY:
-                    neighbor = choice(cell.get_emptiest_neighbors())
-
-                    print(f"{animal} moved: {cell.x, cell.y} -> {neighbor.x, neighbor.y}")
-
-                    cell.animals.remove(animal)
-                    neighbor.animals.add(animal)
-                    # this is ok as we're not changing the keys
-                    self.free_animals[animal] = neighbor
+            self.run_turn(time_elapsed)
--- core/ui/ark_ui.py
+++ core/ui/ark_ui.py
@@ -1,9 +1,122 @@
+import pygame
+
 from core.engine import Engine
+import core.constants as c
 
 
 class ArkUI:
     def __init__(self, engine: Engine) -> None:
+        pygame.init()
+
         self.engine = engine
+        self.running = True
+        self.paused = True
+        self.clock = pygame.time.Clock()
+
+        self.turn = 0
+
+        self.screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+        self.bg_color = c.BG_COLOR
+        self.small_font = pygame.font.Font(None, 20)
+
+    def write_at(self, line: str, coord: tuple[float, float]):
+        text = self.small_font.render(line, True, (0, 0, 0))
+        self.screen.blit(text, coord)
+
+    def draw_grid(self):
+        """Draw garden boundaries and grid."""
+        border_rect = pygame.Rect(
+            c.MARGIN_X, c.MARGIN_Y, c.LANDSCAPE_WIDTH, c.LANDSCAPE_HEIGHT
+        )
+        pygame.draw.rect(self.screen, (0, 0, 0), border_rect, 2)
+
+        for i in range(c.NUM_GRID_LINES + 1):
+            x = c.MARGIN_X + c.LANDSCAPE_WIDTH * i / c.NUM_GRID_LINES
+
+            if i not in [0, c.NUM_GRID_LINES]:
+                # only draw lines inside the grid
+                pygame.draw.line(
+                    self.screen,
+                    c.GRID_COLOR,
+                    (x, c.MARGIN_Y),
+                    (x, c.MARGIN_Y + c.LANDSCAPE_HEIGHT),
+                )
+
+            line = "  X"
+            if i:
+                line = f"{int(c.X * i / c.NUM_GRID_LINES)}"
+            self.write_at(line, (x - 10, c.MARGIN_Y - 20))
+
+        for i in range(c.NUM_GRID_LINES + 1):
+            y = c.MARGIN_Y + c.LANDSCAPE_HEIGHT * i / c.NUM_GRID_LINES
+
+            if i not in [0, c.NUM_GRID_LINES]:
+                pygame.draw.line(
+                    self.screen,
+                    c.GRID_COLOR,
+                    (c.MARGIN_X, y),
+                    (c.MARGIN_X + c.LANDSCAPE_WIDTH, y),
+                )
+
+            line = "  Y"
+            if i:
+                line = f"{int(c.Y * i / c.NUM_GRID_LINES)}"
+            self.write_at(line, (c.MARGIN_X - 30, y - 5))
+
+    def draw_objects(self):
+        # draw ark
+        ark_x, ark_y = self.engine.ark.position
+
+        ark_x_px = c.LANDSCAPE_WEST_PX + c.LANDSCAPE_EAST_PX * ark_x / c.X
+        ark_y_px = c.LANDSCAPE_NORTH_PX + c.LANDSCAPE_SOUTH_PX * ark_y / c.Y
+
+        pygame.draw.circle(self.screen, c.ARK_COLOR, (ark_x_px, ark_y_px), c.ARK_RADIUS)
+
+    def step_simulation(self):
+        """Run one turn of simulation."""
+        if self.turn < self.engine.time:
+            self.engine.run_turn(self.turn)
+            self.turn += 1
+        else:
+            self.paused = True
+
+    def handle_events(self):
+        """Handle pygame events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_q
+            ):
+                self.running = False
+
+            elif event.type == pygame.KEYDOWN:
+                # print(f"event {event.key}")
+                if event.key == pygame.K_SPACE:
+                    print(f"pressed space")
+                    # self.paused = not self.paused
+                elif event.key == pygame.K_RIGHT and self.paused:
+                    print(f"pressed right")
+                    # self.step_simulation()
+                elif event.key == pygame.K_d:  # NEW: Toggle debug mode
+                    print(f"toggle debug mode")
+                #     self.debug_mode = not self.debug_mode
 
     def run(self) -> dict:
+        while self.running:
+            self.handle_events()
+
+            if not self.paused:
+                self.step_simulation()
+                self.clock.tick(10)
+
+            self.screen.fill(self.bg_color)
+            self.draw_grid()
+            self.draw_objects()
+            # self.draw_info_panel()
+            # self.draw_debug_info()
+
+            pygame.display.flip()
+            self.clock.tick(60)
+
+        pygame.quit()
+
         return {}
